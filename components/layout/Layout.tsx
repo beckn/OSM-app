74e2082eb1df8e82c7619fa012ec9ff48a7e042f
kundan singh <kundansingh@kundans-MacBook-Pro.local>
2023-07-03 04:01:01 +0530

    position sticky fixed

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -17,7 +17,7 @@ const Layout: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
   const isHomepage = router.pathname === "/homePage";
   const isSearch = router.pathname === "/search";
   const paddingStyles = "px-5 xl:px-16";
-  const marginStyles = "mt-[106px]";
+  const marginStyles = "mt-[100px]";
 
   return (
     <Provider store={store}>
@@ -40,7 +40,7 @@ const Layout: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
               { [paddingStyles]: !isHomepage },
               { [marginStyles]: !isHomepage && !isSearch },
               { ["mt-[24px]"]: isHomepage },
-              { ["mt-[124px]"]: isSearch }
+              { ["mt-[118px]"]: isSearch }
             )}
           >
             {children}
