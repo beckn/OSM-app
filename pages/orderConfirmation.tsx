8b1340cbf35da7ea21924473019ca09751d9e4b4
Ujjwal Tiwari <ujjwaltiwari@Ujjwals-MacBook-Pro.local>
2023-07-10 23:16:53 +0530

    OR-185:Clearing cart after confirming the order

--- pages/orderConfirmation.tsx
+++ pages/orderConfirmation.tsx
@@ -1,104 +1,123 @@
-import React, { useEffect } from "react";
-import { Box, Image, Stack, Text } from "@chakra-ui/react";
-import { useSelector } from "react-redux";
-import orderConfirmmark from "../public/images/orderConfirmmark.svg";
-import Button from "../components/button/Button";
-import { useLanguage } from "../hooks/useLanguage";
-import useRequest from "../hooks/useRequest";
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { Box, Image, Stack, Text } from '@chakra-ui/react'
+import { useDispatch, useSelector } from 'react-redux'
+import orderConfirmmark from '../public/images/orderConfirmmark.svg'
+import { useLanguage } from '../hooks/useLanguage'
+import useRequest from '../hooks/useRequest'
 import {
-  getInitMetaDataPerBpp,
-  getPayloadForConfirmRequest,
-} from "../utilities/confirm-utils";
-import Loader from "../components/loader/Loader";
-import { TransactionIdRootState } from "../lib/types/cart";
-import { useRouter } from "next/router";
+    getInitMetaDataPerBpp,
+    getPayloadForConfirmRequest,
+} from '../utilities/confirm-utils'
+import Loader from '../components/loader/Loader'
+import { TransactionIdRootState } from '../lib/types/cart'
+import { cartActions } from '../store/cart-slice'
 
 const OrderConfirmation = () => {
-  const { t } = useLanguage();
-  const confirmRequest = useRequest();
-  const router = useRouter();
-  const initResponse = useSelector(
-    (state: any) => state.initResponse.initResponse
-  );
+    const { t } = useLanguage()
+    const confirmRequest = useRequest()
+    const router = useRouter()
+    const initResponse = useSelector(
+        (state: any) => state.initResponse.initResponse
+    )
+    const dispatch = useDispatch()
 
-  const transactionId = useSelector(
-    (state: { transactionId: TransactionIdRootState }) => state.transactionId
-  );
+    const transactionId = useSelector(
+        (state: { transactionId: TransactionIdRootState }) =>
+            state.transactionId
+    )
 
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL
 
-  useEffect(() => {
-    if (initResponse) {
-      const initMetaDataPerBpp = getInitMetaDataPerBpp(initResponse);
+    useEffect(() => {
+        if (initResponse) {
+            const initMetaDataPerBpp = getInitMetaDataPerBpp(initResponse)
 
-      const payLoadForConfirmRequest = getPayloadForConfirmRequest(
-        initMetaDataPerBpp,
-        transactionId,
-        localStorage.getItem("userPhone") as string
-      );
-      confirmRequest.fetchData(
-        `${apiUrl}/client/v2/confirm`,
-        "POST",
-        payLoadForConfirmRequest
-      );
+            const payLoadForConfirmRequest = getPayloadForConfirmRequest(
+                initMetaDataPerBpp,
+                transactionId,
+                localStorage.getItem('userPhone') as string
+            )
+            confirmRequest.fetchData(
+                `${apiUrl}/client/v2/confirm`,
+                'POST',
+                payLoadForConfirmRequest
+            )
+        }
+    }, [])
+
+    useEffect(() => {
+        if (
+            !initResponse &&
+            localStorage &&
+            localStorage.getItem('initResult')
+        ) {
+            const parsedInitResult = JSON.parse(
+                localStorage.getItem('initResult') as string
+            )
+            const initMetaDataPerBpp = getInitMetaDataPerBpp(parsedInitResult)
+
+            const payLoadForConfirmRequest = getPayloadForConfirmRequest(
+                initMetaDataPerBpp,
+                transactionId,
+                localStorage.getItem('userPhone') as string
+            )
+            confirmRequest.fetchData(
+                `${apiUrl}/client/v2/confirm`,
+                'POST',
+                payLoadForConfirmRequest
+            )
+        }
+    }, [])
+
+    useEffect(() => {
+        if (confirmRequest.data) {
+            dispatch(cartActions.clearCart())
+            localStorage.setItem(
+                'confirmData',
+                JSON.stringify(confirmRequest.data)
+            )
+            const timeout = setTimeout(() => {
+                router.push('/orderDetails')
+            }, 3000)
+
+            // Clean up the timeout on component unmount
+            return () => {
+                clearTimeout(timeout)
+            }
+        }
+    }, [confirmRequest.data])
+
+    if (confirmRequest.loading) {
+        return <Loader loadingText={t.confirmingOrderLoader} />
     }
-  }, []);
 
-  useEffect(() => {
-    if (!initResponse && localStorage && localStorage.getItem("initResult")) {
-      const parsedInitResult = JSON.parse(
-        localStorage.getItem("initResult") as string
-      );
-      const initMetaDataPerBpp = getInitMetaDataPerBpp(parsedInitResult);
+    return (
+        <Box position={'relative'}>
+            <Image
+                src={orderConfirmmark}
+                margin={'41px auto'}
+            />
+            <Text
+                fontSize={'17px'}
+                fontWeight={'600'}
+                textAlign={'center'}
+            >
+                {t.orderPlaced}
+            </Text>
+            <Stack>
+                <Text
+                    textAlign={'center'}
+                    marginTop={'8px'}
+                    marginBottom={'15px'}
+                    fontSize={'15px'}
+                >
+                    {t.confirmMessage1} <br />
+                    {t.confirmMessage2}
+                </Text>
+            </Stack>
+        </Box>
+    )
+}
 
-      const payLoadForConfirmRequest = getPayloadForConfirmRequest(
-        initMetaDataPerBpp,
-        transactionId,
-        localStorage.getItem("userPhone") as string
-      );
-      confirmRequest.fetchData(
-        `${apiUrl}/client/v2/confirm`,
-        "POST",
-        payLoadForConfirmRequest
-      );
-    }
-  }, []);
-
-  useEffect(() => {
-    if (confirmRequest.data) {
-      localStorage.setItem("confirmData", JSON.stringify(confirmRequest.data));
-      const timeout = setTimeout(() => {
-        router.push("/orderDetails");
-      }, 3000);
-
-      // Clean up the timeout on component unmount
-      return () => {
-        clearTimeout(timeout);
-      };
-    }
-  }, [confirmRequest.data]);
-
-  if (confirmRequest.loading) {
-    return <Loader loadingText={t.confirmingOrderLoader} />;
-  }
-
-  return (
-    <Box position={"relative"}>
-      <Image src={orderConfirmmark} margin={"41px auto"} />
-      <Text fontSize={"17px"} fontWeight={"600"} textAlign={"center"}>{t.orderPlaced}</Text>
-      <Stack>
-        <Text
-          textAlign={"center"}
-          marginTop={"8px"}
-          marginBottom={"15px"}
-          fontSize={"15px"}
-        >
-          {t.confirmMessage1} <br />
-          {t.confirmMessage2}
-        </Text>
-      </Stack>
-    </Box>
-  );
-};
-
-export default OrderConfirmation;
+export default OrderConfirmation
