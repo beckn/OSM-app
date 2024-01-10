f54fa2871862de48bb364c22e9d4a7b899f9f00e
kundan singh <kundansingh@kundans-MacBook-Pro.local>
2024-01-10 12:54:31 +0530

    fixed pricing issue in order details page

--- components/detailsCard/PaymentDetails.tsx
+++ components/detailsCard/PaymentDetails.tsx
@@ -14,7 +14,6 @@ const PaymentDetails: React.FC<PaymentDetailsProps> = (props) => {
     return (
         <Box>
             {breakup.map((item: any, idx: number) => {
-                console.log(item)
                 return (
                     <Flex
                         key={idx}
